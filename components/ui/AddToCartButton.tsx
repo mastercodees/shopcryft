a2f7ae453a5c181b434d1cbb1950025a8a63b5c6
mastercodees <vladmirnyataige@gmail.com>
2025-06-13 12:43:50 +0300

    added product details

---
+++ components/ui/AddToCartButton.tsx
@@ -0,0 +1,42 @@
+"use client";
+import { Product } from '@/sanity.types';
+import React from 'react'
+import { Button } from './ui/button';
+import { ShoppingBag } from 'lucide-react';
+import { cn } from '@/lib/utils';
+
+interface Props {
+    product: Product;
+    className?: string;
+}
+function AddToCartButton({ product, className }: Props) {
+    const isOutOfStock = product?.stock === 0;
+
+    const handleAddToCart = () => {
+        // if ((product?.stock as number) > itemCount) {
+        //   addItem(product);
+        //   toast.success(
+        //     `${product?.name?.substring(0, 12)}... added successfully!`
+        //   );
+        // } else {
+        //   toast.error("Can not add more than available stock");
+        // }
+        window.alert(`${product?.name} added to cart!`);
+    };
+
+    return (
+        <div>
+            <Button
+                onClick={handleAddToCart}
+                disabled={isOutOfStock}
+                className={cn(
+                    "w-full bg-shop_dark_green/80 shadow-none border border-shop_dark_green/80 font-semibold tracking-wide text-white hover:bg-shop_dark_green hover:border-shop_dark_green hoverEffect",
+                    className
+                )}>
+                <ShoppingBag />{isOutOfStock ? "Out of Stock" : "Add to Cart"}
+            </Button>
+        </div>
+    )
+}
+
+export default AddToCartButton
--- components/ui/HomeTabBar.tsx
+++ components/ui/HomeTabBar.tsx
@@ -10,19 +10,20 @@ interface Props {
 const HomeTabbar = ({ selectedTab, onTabSelect }: Props) => {
 
     return (
-        <div className="flex items-center flex-wrap gap-5 justify-between">
-            <div className="flex items-center gap-1.5 text-sm font-semibold">
-                <div className="flex items-center gap-1.5 md:gap-3">
-                    {productType?.map((item) => (
-                        <button
-                            onClick={() => onTabSelect(item?.title)}
-                            key={item?.title}
-                            className={`border border-shop_light_green/30 px-4 py-1.5 md:px-6 md:py-2 rounded-full hover:bg-shop_light_green hover:border-shop_light_green hover:text-white hoverEffect ${selectedTab === item?.title ? "bg-shop_light_green text-white border-shop_light_green" : "bg-shop_light_green/10"}`}
-                        >
-                            {item?.title}
-                        </button>
-                    ))}
-                </div>
+        <div className="flex items-center justify-between flex-wrap gap-5 ">
+            <div className="flex items-center gap-3 text-sm font-semibold">
+
+                {productType?.map((item) => (
+                    <button
+                        onClick={() => onTabSelect(item?.title)}
+                        key={item?.title}
+                        className={`border border-shop_light_green/30 px-4 py-1.5 md:px-6 md:py-2 rounded-full hover:bg-shop_light_green hover:border-shop_light_green hover:text-white hoverEffect 
+                            ${selectedTab === item?.title ? "bg-shop_light_green text-white border-shop_light_green" : "bg-shop_light_green/10"}`}
+                    >
+                        {item?.title}
+                    </button>
+                ))}
+
             </div>
             <Link
                 href={"/shop"}
